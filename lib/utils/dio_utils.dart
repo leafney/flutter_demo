875e5d110a30a1643fa246a787f5de08d0a897a8
leafney <babycoolzx@126.com>
2021-03-28 17:38:20 +0800

    升级dio到nullsafety

--- lib/utils/dio_utils.dart
+++ lib/utils/dio_utils.dart
@@ -3,6 +3,7 @@
  * @Github     : https://github.com/leafney
  * @Date       : 2021-03-28 16:09:18
  * @Description: dio请求类
+ * dio interceptor : https://github.com/flutterchina/dio/blob/4.0/example/interceptor_lock.dart
  */
 
 import 'dart:io';
@@ -13,18 +14,18 @@ import 'package:flutter_demo/utils/sp_utils.dart';
 import 'package:oktoast/oktoast.dart';
 
 // 用于缓存请求的token，定义为全局变量
-String csrfToken;
+String? csrfToken;
 
 class DioUtils {
-  static DioUtils _instance;
-  Dio _dio;
-  BaseOptions _baseOptions;
+  static DioUtils? _instance;
+  late Dio _dio;
+  late BaseOptions _baseOptions;
 
   static DioUtils getInstance() {
     if (_instance == null) {
       _instance = DioUtils();
     }
-    return _instance;
+    return _instance!;
   }
 
   DioUtils() {
@@ -41,10 +42,10 @@ class DioUtils {
 // 不带access_token的请求
   Future<Response> requestHttp(String url,
       {String method = "get",
-      Map<String, dynamic> params,
-      dynamic data,
-      Options options,
-      CancelToken cancelToken}) async {
+      Map<String, dynamic>? params,
+      dynamic? data,
+      Options? options,
+      CancelToken? cancelToken}) async {
     // 对url做trim处理，防止因为空格等问题出现请求失败
     url = url.trim();
 
@@ -57,7 +58,7 @@ class DioUtils {
     // deviceId拦截器
     _addDeviceIdRequestInterceptor(_dio);
 
-    Response response;
+    late Response response;
     try {
       response = await _dio
           .request(url,
@@ -88,10 +89,10 @@ class DioUtils {
 // 带有access_token的请求
   Future<Response> requestWithToken(String url,
       {String method = "get",
-      Map<String, dynamic> params,
-      Map<String, dynamic> data,
-      Options options,
-      CancelToken cancelToken}) async {
+      Map<String, dynamic>? params,
+      Map<String, dynamic>? data,
+      Options? options,
+      CancelToken? cancelToken}) async {
     // 对url做trim处理，防止因为空格等问题出现请求失败
     url = url.trim();
 
@@ -107,7 +108,7 @@ class DioUtils {
     // 添加自定义token拦截器
     _addTokenInterceptor(_dio);
 
-    Response response;
+    late Response response;
     try {
       response = await _dio
           .request(url,
@@ -132,8 +133,8 @@ class DioUtils {
 
     tokenDio.options = dio.options;
 
-    dio.interceptors
-        .add(InterceptorsWrapper(onRequest: (RequestOptions options) {
+    dio.interceptors.add(InterceptorsWrapper(
+        onRequest: (RequestOptions options, RequestInterceptorHandler hanlder) {
       // 获取当前的Access_Token
       if (csrfToken == null) {
         print('no token,get token first');
@@ -153,13 +154,13 @@ class DioUtils {
       }
 
       // 一定要返回options给后面的操作
-      return options;
-    }, onError: (DioError err) {
+      return hanlder.next(options);
+    }, onError: (DioError err, ErrorInterceptorHandler handler) {
       // 401代表token已过期，需在后台api定义相应错误码
       if (err.response?.statusCode == 401) {
         print('后端返回token已过期的错误码，需要刷新token');
 
-        RequestOptions options = err.response.request;
+        RequestOptions options = err.response!.requestOptions;
 
         // 如果token已被更新，则更新请求中使用的token
         // 这里可以理解成：同时发起了多次请求，但是token过期了。第一次请求执行后会刷新token，但是后面的请求还是使用的之前过期的token，所以这里判断一下token是否一致，更新刷新后的token
@@ -173,7 +174,10 @@ class DioUtils {
           // 更新请求头中的token
           options.headers['Authorization'] = 'Bearer $csrfToken';
           // 重新发起请求
-          return dio.request(options.path, options: options);
+          dio.fetch(options).then((r) => handler.resolve(r), onError: (e) {
+            handler.reject(e);
+          });
+          return;
         }
 
         // 刷新token并重新发起请求
@@ -186,7 +190,7 @@ class DioUtils {
         String refreshToken = SpUtils.getString(ComData.SPKEY_REFRESH_TOEKN);
         print('从本地获取refreshToken: $refreshToken');
         // TODO token的刷新是没有携带device_Id信息的，后期再修改
-        return tokenDio.post(HttpConfig.apiRefreshToken, data: {
+        tokenDio.post(HttpConfig.apiRefreshToken, data: {
           "refresh_token": refreshToken,
         }).then((d) {
           final rspStsCode = d.statusCode;
@@ -203,6 +207,12 @@ class DioUtils {
             //保存新的accessToken
             SpUtils.putString(ComData.SPKEY_ACCESS_TOKEN, accessToken);
           }
+        }).whenComplete(() {
+          print('刷新token成功，解锁请求');
+          // 解锁
+          dio.unlock();
+          dio.interceptors.responseLock.unlock();
+          dio.interceptors.errorLock.unlock();
         }).catchError((e) {
           print('刷新token异常了 ${e.response?.statusCode}');
 
@@ -231,40 +241,39 @@ class DioUtils {
             showToast('信息失效，请重新登录', onDismiss: () {
               // 跳转到登录页不能返回
               // ggg.Get.offNamedUntil(Routes.LOGIN, (route) => false);
+              print('跳转到登录页不能返回');
             });
 
             // 这里需要抛出异常，否则会一直401重试，导致循环重试的问题
             throw "refreshToken timeout";
           }
-        }).whenComplete(() {
-          print('刷新token成功，解锁请求');
-          // 解锁
-          dio.unlock();
-          dio.interceptors.responseLock.unlock();
-          dio.interceptors.errorLock.unlock();
         }).then((e) {
           print('重新发起请求 $e');
           // 重新发起请求
-          return dio.request(options.path, options: options);
+          // return dio.request(options.path, options: options);
+          dio.fetch(options).then((r) => handler.resolve(r), onError: (e) {
+            handler.reject(e);
+          });
         }).catchError((e) {
           print('重新发起请求也失败了 $e');
         });
+        return;
       }
-      return err;
+      return handler.next(err);
     }));
   }
 
 // 要来捕获dio的异常信息
   _handleError(dynamic err) {
-    Response response = err?.response;
+    Response? response = err?.response;
     if (response != null) {
       print('拦截器捕获*异常*返回结果：$response');
 
-      if (response.statusCode >= 400 && response.statusCode < 500) {
+      if (response.statusCode! >= 400 && response.statusCode! < 500) {
         // 获取返回信息中的message并弹窗提示
         var errMsg = response.data['message'] ?? "";
         showToast(errMsg);
-      } else if (response.statusCode >= 500) {
+      } else if (response.statusCode! >= 500) {
         showToast('系统繁忙，请稍后重试');
       }
     }
@@ -273,13 +282,14 @@ class DioUtils {
 // 请求及返回数据拦截器
   _addResponseInterceptor(Dio dio) {
     dio.interceptors.add(
-      InterceptorsWrapper(onRequest: (RequestOptions options) {
+      InterceptorsWrapper(onRequest:
+          (RequestOptions options, RequestInterceptorHandler handler) {
         print(
             '发起请求:[ ${options.baseUrl}${options.path} ],请求数据：[ ${options.data.toString()} ]');
-        return options;
-      }, onResponse: (Response response) {
+        return handler.next(options);
+      }, onResponse: (Response response, ResponseInterceptorHandler handler) {
         print('拦截器捕获*正常*返回结果：$response');
-        return response;
+        return handler.next(response);
       }),
     );
   }
@@ -287,12 +297,13 @@ class DioUtils {
   // 请求头DeviceId拦截器
   _addDeviceIdRequestInterceptor(Dio dio) {
     // dio.interceptors.add(
-    //   InterceptorsWrapper(onRequest: (RequestOptions options) {
+    //   InterceptorsWrapper(onRequest:
+    //       (RequestOptions options, RequestInterceptorHandler handler) {
     //     // 从本地存储中获取当前设备Id,添加到请求头信息中
     //     String deviceId =
     //         SpUtils.getString(CommonUtils.SPKEY_DEVICE_ID, defValue: "");
     //     options.headers['deviceId'] = deviceId;
-    //     return options;
+    //     return handler.next(options);
     //   }),
     // );
   }
@@ -300,7 +311,7 @@ class DioUtils {
 
 /*
 仍存在的问题：
-1. 【已解决】 -- 当token过期后，如果同时发起多条请求，那么每条请求都会去执行更新token的操作。理想中应该是发现token过期，第一条请求会执行更新token的操作，后面的请求则直接使用更新后的token发起请求
+1. 【待验证】 -- 当token过期后，如果同时发起多条请求，那么每条请求都会去执行更新token的操作。理想中应该是发现token过期，第一条请求会执行更新token的操作，后面的请求则直接使用更新后的token发起请求
 2. 【待处理】 -- 无网的情况下跳转到无网提示页面
 3. 【待处理】 -- 请求时弹出加载中弹窗
 
